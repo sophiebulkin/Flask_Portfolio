75ec37f419a8bcf80ea865f2054a0c7041ffb0da
1855532 <72881866+1855532@users.noreply.github.com>
2020-10-30 17:37:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,40 @@ def contents():
     #Flask import uses Jinga to render HTML
     return render_template("contents.html")
 
+@app.route('/aboutdn/')
+def delnorte():
+    #Flask import uses Jinga to render HTML
+    return render_template("aboutdn.html")
+
+@app.route('/aboutsd/')
+def sandiego():
+    #Flask import uses Jinga to render HTML
+    return render_template("aboutsd.html")
+
+@app.route('/playground/')
+def playground():
+    #Flask import uses Jinga to render HTML
+    return render_template("dropdown.html")
+
+@app.route('/aboutsd/history/')
+def sdhistory():
+    #Flask import uses Jinga to render HTML
+    return render_template("sdhistory.html")
+
+@app.route('/aboutsd/landmarks/')
+def sdlandmarks():
+    #Flask import uses Jinga to render HTML
+    return render_template("sdlandmarks.html")
+
+@app.route('/aboutsd/tourist/')
+def sdtourist():
+    #Flask import uses Jinga to render HTML
+    return render_template("sdtourist.html")
+
+@app.route('/aboutsd/restaurant/')
+def sdrestaurant():
+    #Flask import uses Jinga to render HTML
+    return render_template("sdrestaurant.html")
 
 if __name__ == "__main__":
     #runs the application on the repl development server
